9f56b179fe7107f8ba9ed1f47af3ecc87a341be1
Gurps Bassi <gurpiar.bassi@gmail.com>
2018-08-02 23:44:33 +0100

    Demonstration of avro serialisation
    - Serialise data to and from a flat file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # avro-java-sample
 playground for avro with java
+
+running mvn compile should generate the Person.java class in the target directory along with its builder.
---
+++ src/test/java/TestAvro.java
@@ -0,0 +1,64 @@
+import com.gurps.avro.Person;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class TestAvro {
+
+    static final String FILENAME = "person-test.avro";
+
+    @Test
+    void roundtripSerialisation() {
+        Person person = Person.newBuilder()
+                              .setId(1)
+                              .setFirstName("fred")
+                              .setLastName("flintstone")
+                              .setEmailAddress("fred.flintstone@bedrock.com")
+                              .setJoinDate("2017-06-01")
+                              .setBirthdate("1982-01-12")
+                              .setPhoneNumber("91822")
+                              .setMiddleName("bernard")
+                              .setSiblings(2)
+                              .setUsername("fredflintstone")
+                              .build();
+
+        writeToDisk(person);
+        readFromDisk();
+    }
+
+    private void writeToDisk(Person person) {
+        File output = new File(FILENAME);
+        try {
+            DatumWriter<Person> personDatumWriter = new SpecificDatumWriter<>(Person.class);
+            DataFileWriter<Person> dataFileWriter = new DataFileWriter<>(personDatumWriter);
+            dataFileWriter.create(person.getSchema(), output);
+            dataFileWriter.append(person);
+            dataFileWriter.append(person);
+            dataFileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Error writing Avro");
+        }
+    }
+
+    private void readFromDisk() {
+        try {
+            DatumReader<Person> personDatumReader = new SpecificDatumReader(Person.class);
+            DataFileReader<Person> dataFileReader = new DataFileReader<>(new File(FILENAME), personDatumReader);
+            Person person = null;
+            while (dataFileReader.hasNext()) {
+                person = dataFileReader.next(person);
+                System.out.println(person);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading Avro");
+        }
+    }
+}
